669949ef53e9025d81f3438e107e6c9bc18c7984
João Victor Santos <88259854+joonzinho@users.noreply.github.com>
2022-08-08 18:49:25 -0300

    Update Desafio Longest Word - Coderbyte.py

--- Desafio Longest Word - Coderbyte.py
+++ Desafio Longest Word - Coderbyte.py
@@ -6,15 +6,17 @@ import re
 # Função do LongestWord utilizando como parâmetro (sen)
 def LongestWord(sen):
   count = 0
-  # Retirar pontuação usando o REGEX
+  # Retirar pontuação usando a bibioteca RE
   sen_tratada = re.sub(r'[^\w\s]','',sen)
-  # Separa as palavras em "" que estão na frase
+  # Separa as palavras em "" que estão na frase - LISTA
   lista_de_palavras = sen_tratada.split()
+  # Função para verificar qual a maior palavra da frase - LISTA
   for palavra in lista_de_palavras:
       if len(palavra) > count:
-           count = len(palavra)
-           maior_palavra = palavra
-  return maior_palavra
+          count = len(palavra)
+          palavra_maior = palavra
+    # Retorna a maior palavra da frase
+  return palavra_maior
 
 # Deixar a função de chamada abaixo
 print(LongestWord(input()))
